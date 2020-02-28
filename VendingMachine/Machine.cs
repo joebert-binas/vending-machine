dd7555d0e5f09cea85442f9de94fdf9f10f5c23b
joebert.binas <joebert.binas@devpartners.co>
2020-02-28 20:49:23 +0800

    Initial Commit

---
+++ VendingMachine/Machine.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Transactions;
+
+namespace VendingMachine
+{
+    public class Machine
+    { 
+        public List<Product> SelectedProducts { get; set; }
+        public List<double> Payments { get; set; }
+
+        public Machine()
+        {
+            SelectedProducts = new List<Product>();
+            Payments = new List<double>();
+        }
+
+        public List<Product> GetProducts()
+        {
+           return new List<Product>()
+            {
+                new Product{Id = 1, Name = "Coke", Price = 25},
+                new Product{Id = 2, Name = "Pepsi", Price = 35},
+                new Product{Id = 3, Name = "Soda", Price = 45},
+                new Product{Id = 4, Name = "Chocolate Bar", Price = 20.25},
+                new Product{Id = 5, Name = "Chewing Gum", Price = 10.50},
+                new Product{Id = 6, Name = "Bottled Water", Price = 15},
+            };  
+        }
+
+        public void SelectProduct(int productId)
+        {
+            var product = GetProducts().FirstOrDefault(x => x.Id == productId);
+
+            if (product != null)
+            {
+               SelectedProducts.Add(product);
+            }
+             
+        }
+
+        public void InsertPayment(double payment)
+        {
+            if (IsValidPayment(payment))
+            {
+                Payments.Add(payment);
+            }
+        }
+ 
+        public bool IsValidPayment(double payment)
+        {
+            var validDenomination = new List<double> { 100, 50, 20, 10, 5, 1, .50, .25 };
+
+            return validDenomination.Any(x => x.Equals(payment));
+        }
+
+        public double CancelRequest()
+        {
+            var totalPayments = Payments.Sum();
+
+            ClearRequest(); 
+
+            return totalPayments;
+        }
+
+        public void ClearRequest()
+        {
+            SelectedProducts.Clear();
+            Payments.Clear(); 
+        }
+
+        public double GetAmountDue()
+        {
+            return SelectedProducts.Sum(x => x.Price); 
+        }
+
+        public double GetTotalPayment()
+        {
+            return Payments.Sum(); 
+        } 
+        public double GetChange()
+        {
+            return GetTotalPayment() - GetAmountDue();
+        }
+         
+        public string RunRequest()
+        {
+            var due = GetAmountDue();
+            var payment = GetTotalPayment();
+
+            if (due > payment)
+            {
+                return "Inserted money is insufficient to the selected product";
+            }
+
+            return string.Empty;
+        }
+
+
+
+
+
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+}
---
+++ VnedingMachine.Tests/VendingMachine.Tests.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using NUnit.Framework;
+using Shouldly;
+using VendingMachine;
+namespace Tests
+{
+    public class VendingMachineTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase(10)]
+        [TestCase(5)]
+        [TestCase(1)]
+        [TestCase(.50)]
+        [TestCase(.25)]
+        public void VendingMachine_InsertPayment_WhenCoinIsValid(double coin)
+        {
+            var machine = new Machine();
+            machine.InsertPayment(coin);
+
+            var result = machine.Payments.Any();
+
+            result.ShouldBeTrue();
+        }
+
+        [Test]
+        [TestCase(100)]
+        [TestCase(50)]
+        [TestCase(20)] 
+        public void VendingMachine_InsertPayment_WhenBillIsValid(double bill)
+        {
+            var machine = new Machine();
+            machine.InsertPayment(bill);
+
+            var result = machine.Payments.Any();
+
+            result.ShouldBeTrue();
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void VendingMachine_SelectProduct_WhenProductIdIsValid(int productId)
+        {
+            var machine = new Machine();
+            machine.SelectProduct(productId);
+
+            var result = machine.SelectedProducts.Any();
+
+            result.ShouldBeTrue(); 
+        }
+
+        [Test]
+        [TestCase(-1, 0)]
+        [TestCase(0, 0)]
+        public void VendingMachine_Should_Ignore_InvalidSelectedProduct(int productId, int expected)
+        {
+            var machine = new Machine();
+            machine.SelectProduct(productId);
+
+            var result = machine.SelectedProducts.Count();
+
+            expected.ShouldBe(result);
+        }
+
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void VendingMachine_Should_Return_GreaterThanZeroSelectedProduct(int productId)
+        {
+            var machine = new Machine();
+            machine.SelectProduct(productId);
+
+            var price = machine.SelectedProducts.First().Price;
+
+            price.ShouldBeGreaterThan(0);
+        }
+
+        [Test]
+        [TestCase(1, 100,100 )]
+        [TestCase(2, 50, 50)]
+        public void VendingMachine_Should_Return_Refund_WhenCancellingRequest(int productId, double payment, double expected)
+        {
+            var machine = new Machine();
+            machine.SelectProduct(productId);
+            machine.InsertPayment(payment);
+             
+            var result = machine.CancelRequest();
+
+            expected.ShouldBe(result);
+        }
+
+        [Test]
+        [TestCase(1, 1)]
+        [TestCase(2, 2)]
+        public void VendingMachine_Should_Return_SelectedProduct(int productId, int expected)
+        {
+            var machine = new Machine();
+            machine.SelectProduct(productId); 
+
+            var result = machine.SelectedProducts.First();
+
+            expected.ShouldBe(result.Id);
+        }
+
+        [Test]
+        [TestCase(1, new double [] {50 }, 25)]
+        [TestCase(2, new double[] { 100 }, 65)]
+        [TestCase(3, new double[] { 50 }, 5)]
+        [TestCase(4, new double[] { 10,10, 5 }, 4.75)]
+        [TestCase(4, new double[] {20, 1 }, 0.75)]
+        [TestCase(5, new double[] { 5, 5, .50 }, 0)]
+        [TestCase(6, new double[] {20 }, 5)]
+        public void VendingMachine_Should_Return_RemainingChange(int productId, double[] payments, double expected)
+        {
+            var machine = new Machine();
+            machine.SelectProduct(productId);
+
+            foreach (var payment in payments)
+            {
+                machine.InsertPayment(payment);
+            } 
+
+            machine.RunRequest();
+
+            var change = machine.GetChange();
+
+            expected.ShouldBe(change);
+        }
+
+
+        [Test] 
+        [TestCase(1, new double[] { 20 },  "Inserted money is insufficient to the selected product")]
+        public void VendingMachine_Should_Return_Message_IfInsertedMoneyIsInsufficient(int productId, double[] payments, string expected)
+        {
+            var machine = new Machine();
+            machine.SelectProduct(productId);
+
+            foreach (var payment in payments)
+            {
+                machine.InsertPayment(payment);
+            }
+
+            var result = machine.RunRequest();
+             
+            expected.ShouldBe(result);
+
+        }
+
+
+    }
+}
